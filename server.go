fb2997d2f056a1f74d517b7720590adad770c24c
NotTomora <filmrockk@gmail.com>
2022-12-26 21:13:21 +0700

    graceful shutdown

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"log"
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/chewwey/assessment/user"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 	_ "github.com/lib/pq"
 )
 
@@ -13,10 +18,26 @@ func main() {
 	user.InitDB()
 
 	e := echo.New()
-
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Logger.SetLevel(log.INFO)
 	e.POST("/expenses", user.CreateExpensesHandler)
 	e.GET("/expenses/:id", user.GetExpensesByIdHandler)
 	e.PUT("/expenses/:id", user.UpdateByIdHandler)
 	e.GET("/expenses", user.GetAllUserHandler)
-	log.Fatal(e.Start(os.Getenv("PORT")))
+
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
